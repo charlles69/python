5b752941e67ced35ac0756a7a825ce242b8a2c0e
charlles69 <146854189+charlles69@users.noreply.github.com>
2024-10-15 12:03:20 -0300

    fiz

---
+++ python.py
@@ -0,0 +1,4 @@
+print("<-------------------------------------------->\n|                                           |\n| CADASTRO DE DADOS DO USUÁRIO              |\n|                                           |\n<-------------------------------------------->\n")
+nome = input ("Digite seu nome: \n")
+idade = input ("Digite sua idade: \n")
+cpf = input ("Digite o seu Cpf: \n")
